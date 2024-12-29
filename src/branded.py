16685e83f1e982fd8271629a8cb151af2b76f15f
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-12-29 21:26:37 +0530

    branded terms and insights

---
+++ src/branded.py
@@ -0,0 +1,27 @@
+# Importing Dependencies
+import warnings
+import pandas as pd
+from collections import Counter
+warnings.filterwarnings('ignore')
+
+def detect_brand_term(sheet):
+    """
+    Function to detect branded terms
+    """
+    df = pd.read_excel(sheet, sheet_name='Queries')
+    df = df.head(10)
+    # Count the frequency of each word
+    words = df['Top queries'].str.split().sum()
+    word_freq = Counter(words)
+    # Get the brand term
+    brand_term = word_freq.most_common(1)[0][0]
+    return brand_term
+
+def branded_traffic(sheet, brand_term):
+    """
+    Function to get the traffic from branded terms
+    """
+    df = pd.read_excel(sheet, sheet_name='Queries')
+    branded_clicks = df[df['Top queries'].str.contains(brand_term, case=False)]['Clicks'].sum()
+    branded_impressions = df[df['Top queries'].str.contains(brand_term, case=False)]['Impressions'].sum()
+    return branded_clicks, branded_impressions
