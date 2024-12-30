4e418ca8140cee186d7c5ef130668c8eeb4ad108
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-12-30 20:57:24 +0530

    preprocessing of data

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,9 @@ import warnings
 import pandas as pd
 import pycountry
 
+from .branded import detect_brand_term
+warnings.filterwarnings('ignore')
+
 def format_number(number):
     """
     Format a number to a human-readable string.
@@ -30,3 +33,25 @@ def country_code(country_name):
         return country.alpha_2  # Return the ISO 3166-1 alpha-2 code
     except LookupError:
         return None
+
+def preprocessing_queries(data):
+    """
+    Function to preprocess the data
+    """
+    brand_term = detect_brand_term(data)
+    queries_sheet = pd.read_excel(data, sheet_name='Queries')
+    queries_sheet["Branded"] = queries_sheet["Top queries"].apply(lambda x: 1 if brand_term in x else 0)
+    non_branded = queries_sheet[queries_sheet["Branded"] == 0]
+    non_branded['Position'] = non_branded['Position'].round(0).astype(int)
+    return non_branded
+
+def preprocessing_dates(data):
+    """
+    Function to preprocess the dates
+    """
+    df = pd.read_excel(data, sheet_name='Dates')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Month'] = df['Date'].dt.month
+    df['Year'] = df['Date'].dt.year
+    df = df.groupby('Date')['Clicks'].sum()
+    return df
